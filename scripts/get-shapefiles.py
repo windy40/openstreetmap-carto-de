d496df479639b5d54a9a1c7a043f2d4e41d904de
Sven Geggus <sven-osm@geggus.net>
2019-06-07 10:26:25 +0200

    low-zoom water polygons are currently unavailable

--- scripts/get-shapefiles.py
+++ scripts/get-shapefiles.py
@@ -86,29 +86,29 @@ settings = {
         'long_opt': '--icesheet-outlines'
     },
 
-    7: {
-        'directory': 'ocean-polygons-reduced-3857',
-        'url': 'http://data.openstreetmapdata.com/ocean-polygons-reduced-3857.zip',  # noqa
-        'type': 'zip',
-        'shp_basename': ['ocean_reduced_z0', 'ocean_reduced_z1', 'ocean_reduced_z2', 'ocean_reduced_z3', 'ocean_reduced_z4', 'ocean_reduced_z5', 'ocean_reduced_z6'],
-        'long_opt': '--ocean-polygons-reduced'
-    },
+    #7: {
+    #    'directory': 'ocean-polygons-reduced-3857',
+    #    'url': 'http://data.openstreetmapdata.com/ocean-polygons-reduced-3857.zip',  # noqa
+    #    'type': 'zip',
+    #    'shp_basename': ['ocean_reduced_z0', 'ocean_reduced_z1', 'ocean_reduced_z2', 'ocean_reduced_z3', 'ocean_reduced_z4', 'ocean_reduced_z5', 'ocean_reduced_z6'],
+    #    'long_opt': '--ocean-polygons-reduced'
+    #},
 
-    8: {
-        'directory': 'lakes-polygons-reduced-3857',
-        'url': 'http://data.openstreetmapdata.com/lakes-polygons-reduced-3857.zip',  # noqa
-        'type': 'zip',
-        'shp_basename': ['lakes_reduced_z0', 'lakes_reduced_z1', 'lakes_reduced_z2', 'lakes_reduced_z3', 'lakes_reduced_z4', 'lakes_reduced_z5', 'lakes_reduced_z6'],
-        'long_opt': '--lakes-polygons-reduced'
-    },
+    #8: {
+    #    'directory': 'lakes-polygons-reduced-3857',
+    #    'url': 'http://data.openstreetmapdata.com/lakes-polygons-reduced-3857.zip',  # noqa
+    #    'type': 'zip',
+    #    'shp_basename': ['lakes_reduced_z0', 'lakes_reduced_z1', 'lakes_reduced_z2', 'lakes_reduced_z3', 'lakes_reduced_z4', 'lakes_reduced_z5', 'lakes_reduced_z6'],
+    #    'long_opt': '--lakes-polygons-reduced'
+    #},
 
-    9: {
-        'directory': 'river-polygons-reduced-3857',
-        'url': 'http://data.openstreetmapdata.com/river-polygons-reduced-3857.zip',  # noqa
-        'type': 'zip',
-        'shp_basename': ['river_reduced_z0', 'river_reduced_z1', 'river_reduced_z2', 'river_reduced_z3', 'river_reduced_z4', 'river_reduced_z5', 'river_reduced_z6'],
-        'long_opt': '--river-polygons-reduced'
-    }
+    #9: {
+    #    'directory': 'river-polygons-reduced-3857',
+    #    'url': 'http://data.openstreetmapdata.com/river-polygons-reduced-3857.zip',  # noqa
+    #    'type': 'zip',
+    #    'shp_basename': ['river_reduced_z0', 'river_reduced_z1', 'river_reduced_z2', 'river_reduced_z3', 'river_reduced_z4', 'river_reduced_z5', 'river_reduced_z6'],
+    #    'long_opt': '--river-polygons-reduced'
+    #}
 }
 
 u_prompt = True
